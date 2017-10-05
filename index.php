51414e43e5a9807da1b5a68b6581098697f93807
José Luis Ledesma <joseluis.ledesma@socialpoint.es>
2017-10-05 18:16:31 +0200

    test

--- index.php
+++ index.php
@@ -12,13 +12,10 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 // Capturar la URL y pasarla al navegador
 $data = curl_exec($ch);
 curl_close($ch);
-echo $data;
 $json = json_decode($data, true);
-echo $json['items'];
 
-//echo $json;
-/*foreach($json['item'] as $item) {
+foreach($json['items'] as $item) {
   echo $item['metadata']['name']. "<br/>";
-}*/
+}
 
 // Cerrar el recurso cURL y liberar recursos del sistema
