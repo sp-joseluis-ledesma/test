44931405628550e8a27eba46cc28328f75101d0f
José Luis Ledesma <joseluis.ledesma@socialpoint.es>
2017-10-05 18:03:41 +0200

    list

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?
-echo "hello Guillem<br />";
+<?php
+// Crear un nuevo recurso cURL
+$ch = curl_init();
+
+// Establecer URL y otras opciones apropiadas
+curl_setopt($ch, CURLOPT_URL, "https://admin:qiJxHVNAmUlWGodxo99IYa38ICIvYG9a@kubernetes.default.svc.cluster.local/api/v1/namespaces/");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+// Capturar la URL y pasarla al navegador
+curl_exec($ch);
+
+// Cerrar el recurso cURL y liberar recursos del sistema
+curl_close($ch);
