96fa97fc8ddd18528230ec89c0efbff058e5b4e4
José Luis Ledesma <joseluis.ledesma@socialpoint.es>
2017-10-05 18:16:09 +0200

    test

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($ch);
 curl_close($ch);
 echo $data;
-$json = json_decode($data);
+$json = json_decode($data, true);
 echo $json['items'];
 
 //echo $json;
